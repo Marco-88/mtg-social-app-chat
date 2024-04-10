6cb120e66538d890da17efd6c0c9715d0692ca23
Marco <s66853@beuth-hochschule.de>
2024-04-10 19:14:59 +0200

    Add basic info pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+
+const NotFound = () => {
+    return <div>
+        <h2>Page not found</h2>
+        <p>Sorry the page you are looking for does not exist</p>
+        <Link href="/">Return to start page</Link>
+    </div>
+}
+
+export default NotFound
